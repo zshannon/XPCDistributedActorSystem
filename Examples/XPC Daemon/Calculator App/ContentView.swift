683e2729db58b4d3cea82f82d8063df5c4e3fd44
Andreas Ley <andreas@andreasley.ch>
2024-12-08 21:20:34 +0100

    UI enhancements

--- Examples/XPC Daemon/Calculator App/ContentView.swift
+++ Examples/XPC Daemon/Calculator App/ContentView.swift
@@ -13,80 +13,100 @@ struct ContentView: View
     @State private var daemonStatus = "Unknown"
 
     var body: some View {
-        Form {
-            Section("Important") {
-                Label("This app installs a daemon into your system that will keep running.\rMake sure to uninstall it when you're done testing!", systemImage: "exclamationmark.triangle")
+        VStack {
+            VStack(alignment: .leading, spacing: 10) {
+                Label("Important", systemImage: "exclamationmark.triangle")
+                    .bold()
+                    .font(.title)
+                Text("If you click \"Install Launch Daemon\", a LaunchDaemon process with root privileges will be installed and started. This process will be kept running and automatically restarted if you don't uninstall it by clicking \"Uninstall Launch Daemon\".")
+                Text("Make sure to uninstall it when you're done testing.")
             }
-            Section("Service Management (Deamon)") {
-                LabeledContent("Service name") {
-                    Text("\(service)")
-                }
-                LabeledContent("Status") {
-                    HStack {
-                        Text(daemonStatus)
-                        if service.status == .requiresApproval {
-                            Button("Open System Settings") {
-                                SMAppService.openSystemSettingsLoginItems()
-                            }
-                        }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .foregroundStyle(.white)
+            .background(.orange)
+            Form {
+                Section("Service Management (Deamon)") {
+                    LabeledContent("Service name") {
+                        Text("\(service)")
                     }
-                }
-                LabeledContent("Actions") {
-                    HStack {
-                        Button("Install Launch Daemon") {
-                            do {
-                                try service.register()
-                            } catch {
-                                print("Unable to register \(error)")
-                            }
-                            updateDaemonStatus()
-                        }
-                        .disabled(service.status != .notRegistered)
-                        Button("Uninstall Launch Daemon") {
-                            do {
-                                try service.unregister()
-                            } catch {
-                                print("Unable to register \(error)")
-                            }
-                            updateDaemonStatus()
-                        }
-                        .disabled(service.status != .enabled && service.status != .requiresApproval)
-                    }
-                }
-            }
-            Section("XPC") {
-                LabeledContent("Service identifier") {
-                    Text("\(daemonXPCServiceIdentifier)")
-                }
-                LabeledContent("Actions") {
-                    HStack {
-                        Button("Calculate") {
-                            guard let calculator else {
-                                output = "Distributed actor not set up"
-                                return
-                            }
-                            output = "Calculating..."
-                            Task {
-                                do {
-                                    let number1 = Int.random(in: 0..<1000)
-                                    let number2 = Int.random(in: 0..<1000)
-                                    let result = try await calculator.add(number1, number2)
-                                    output = "\(number1) + \(number2) = \(result)"
-                                } catch {
-                                    output = "Failed to calculate: \(error.localizedDescription)"
+                    LabeledContent("Status") {
+                        HStack {
+                            Text(daemonStatus)
+                            if service.status == .requiresApproval {
+                                Button("Open System Settings") {
+                                    SMAppService.openSystemSettingsLoginItems()
                                 }
                             }
                         }
                     }
+                    LabeledContent("Actions") {
+                        HStack {
+                            Button("Install Launch Daemon") {
+                                do {
+                                    try service.register()
+                                } catch {
+                                    print("Unable to register \(error)")
+                                }
+                                updateDaemonStatus()
+                            }
+                            .disabled(service.status != .notRegistered)
+                            Button("Uninstall Launch Daemon") {
+                                do {
+                                    try service.unregister()
+                                } catch {
+                                    print("Unable to register \(error)")
+                                }
+                                updateDaemonStatus()
+                            }
+                            .disabled(service.status != .enabled && service.status != .requiresApproval)
+                        }
+                    }
                 }
-                LabeledContent("Response") {
-                    Text(output)
+                Section("XPC") {
+                    LabeledContent("Service identifier") {
+                        Text("\(daemonXPCServiceIdentifier)")
+                    }
+                    LabeledContent("Actions") {
+                        HStack {
+                            Button("Calculate") {
+                                guard let calculator else {
+                                    output = "Distributed actor not set up"
+                                    return
+                                }
+                                output = "Calculating..."
+                                Task {
+                                    do {
+                                        let number1 = Int.random(in: 0..<1000)
+                                        let number2 = Int.random(in: 0..<1000)
+                                        let result = try await calculator.add(number1, number2)
+                                        output = "\(number1) + \(number2) = \(result)"
+                                    } catch {
+                                        output = "Failed to calculate: \(error.localizedDescription)"
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    LabeledContent("Response") {
+                        Text(output)
+                    }
+                }
+            }
+            .formStyle(.grouped)
+            .padding()
+        }
+        .onAppear(perform: updateDaemonStatus)
+        .task {
+            while true {
+                // TODO: Find a better way to check the service status than this loop
+                try? await Task.sleep(for: .seconds(1), tolerance: .seconds(1))
+                let newDaemonStatus = service.status.description
+                if newDaemonStatus != daemonStatus {
+                    updateDaemonStatus()
                 }
             }
         }
-        .formStyle(.grouped)
-        .padding()
-        .onAppear(perform: updateDaemonStatus)
     }
     
     func updateDaemonStatus()
